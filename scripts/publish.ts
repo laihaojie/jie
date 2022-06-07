5912f59426e33d8f2f64459fc71f415bf7bff273
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-07 15:27:29 +0800

    chore: update

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -9,12 +9,13 @@ const release = process.argv[2]
 
 !release && execSync('npm run build', { stdio: 'inherit' })
 
+execSync("nrm use npm", { stdio: 'inherit' })
+
 let command = 'npm publish --access public'
 
 if (version.includes('beta'))
   command += ' --tag beta'
 
-execSync("nrm use npm", { stdio: 'inherit' })
 
 for (const { name } of packages) {
   execSync(command, { stdio: 'inherit', cwd: path.join('packages', name, 'dist') })
