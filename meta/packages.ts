50b4319fefdb81367107a6dd7bfd2c39558c669f
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-07 08:42:42 +0800

    chore: update meta

---
+++ meta/packages.ts
@@ -0,0 +1,10 @@
+export const packages = [
+  {
+    name: 'utils',
+    description: 'The utils of djie',
+  },
+  {
+    name: 'ws',
+    description: 'The ws of djie',
+  },
+]
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,15 +1,11 @@
 import { execSync } from 'child_process'
 
-const package_name = 'utils'
-
-const packages = [
-  "utils",
-  "ws",
-]
+import { packages } from "../meta/packages"
 
 
 
-for (const name of packages) {
+
+for (const { name, description } of packages) {
   const command = `npx tsup packages/${name}/index.ts --dts --format cjs,esm,iife -d packages/${name}/dist`
   execSync(command, { stdio: 'inherit' })
 }
