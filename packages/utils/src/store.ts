5a4913b012f0fd3be9a67174b684bbba2a034fdd
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-08 08:41:29 +0800

    feat: add sotre

---
+++ packages/utils/src/store.ts
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {String} key 
+ * @returns 
+ */
+export function localGet(key: string) {
+  try {
+    return JSON.parse(window.localStorage.getItem(key) as string)
+  } catch (error) {
+    return window.localStorage.getItem(key)
+  }
+}
+
+export function localSet(key: string, value: any) {
+  window.localStorage.setItem(key, JSON.stringify(value))
+}
+
+export function localRemove(key: string) {
+  window.localStorage.removeItem(key)
+}
