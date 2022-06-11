5d386920db9b9b252aaf6f5c3f10e18bb5d619d4
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-11 09:27:21 +0800

    chore: add rollup

---
+++ scripts/rollup.config.ts
@@ -0,0 +1,11 @@
+import type { OutputOptions, Plugin, RollupOptions } from 'rollup'
+import { packages } from "../meta/packages"
+
+const configs: RollupOptions[] = []
+
+for (const { name } of packages) {
+
+
+}
+
+export default configs
