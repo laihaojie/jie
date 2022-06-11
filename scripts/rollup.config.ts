d223ec35e5685d6fdbe8a716ce205ec73317120b
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-11 10:17:19 +0800

    chore: update

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -24,6 +24,7 @@ for (const { name, mjs, cjs, dts, external } of packages) {
     output.push({
       file: `packages/${name}/dist/index.cjs`,
       format: 'cjs',
+      exports: 'auto'
     })
   }
 
