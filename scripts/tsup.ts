616c7c934d02e99a2a5a290d69287c2457f54518
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-12 17:51:41 +0800

    choer: update

--- scripts/tsup.ts
+++ scripts/tsup.ts
@@ -3,6 +3,7 @@ import { packages } from "../meta/packages"
 
 
 for (const { name, description } of packages) {
+  // const command = `npx tsup packages/${name}/index.ts --dts --format cjs,esm -d packages/${name}/dist --external vue`
   const command = `npx tsup packages/${name}/index.ts --dts --format cjs,esm -d packages/${name}/dist`
   execSync(command, { stdio: 'inherit' })
 }
