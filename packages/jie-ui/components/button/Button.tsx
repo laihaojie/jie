35eb22a863d5ae0a0a8274c9c1f9c0be1a2b4f28
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-21 08:33:01 +0800

    chore: update eslint

--- packages/jie-ui/components/button/Button.tsx
+++ packages/jie-ui/components/button/Button.tsx
@@ -10,7 +10,7 @@ export default defineComponent({
   },
   setup(props) {
     return () => (
-      <button>这个一个按钮{props.title}</button>
+      <button class="bg-red">这个一个按钮{props.title}</button>
     )
   },
 })
