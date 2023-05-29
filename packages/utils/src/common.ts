5c9a12cf2fcf9366576beffac94ea3fa15ef6e0d
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-05-29 11:58:26 +0800

    refactor: utils

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -15,44 +15,12 @@ export function randomInt(length = 6): number {
   return Number(code)
 }
 
-export function rafThrottle(fn: Function) {
-  let locked = false
-  return function (this: any, ...args: any[]) {
-    if (locked)
-      return
-    locked = true
-    window.requestAnimationFrame(() => {
-      fn.apply(this, args)
-      locked = false
-    })
-  }
-}
-
 export function getUuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = Math.random() * 16 | 0
     const v = c === 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   })
-}
-
-/**
- * @param {string} url
- * @returns {Object}
- */
-export function getQueryObject(url: string) {
-  url = url == null ? window.location.href : url
-  const search = url.substring(url.lastIndexOf('?') + 1)
-  const obj: any = {}
-  const reg = /([^?&=]+)=([^?&=]*)/g
-  search.replace(reg, (rs, $1, $2) => {
-    const name = decodeURIComponent($1)
-    let val = decodeURIComponent($2)
-    val = String(val)
-    obj[name] = val
-    return rs
-  })
-  return obj
 }
 
 /**
@@ -149,16 +117,6 @@ export function param2Obj(url: string) {
 }
 
 /**
- * @param {string} val
- * @returns {string}
- */
-export function html2Text(val: string) {
-  const div = document.createElement('div')
-  div.innerHTML = val
-  return div.textContent || div.innerText
-}
-
-/**
  * Merges two objects, giving the last one precedence
  * @param {Object} target
  * @param {(Object|Array)} source
@@ -179,27 +137,6 @@ export function objectMerge(target: any, source: any) {
       target[property] = sourceProperty
   })
   return target
-}
-
-/**
- * @param {HTMLElement} element
- * @param {string} className
- */
-export function toggleClass(element: HTMLElement, className: string) {
-  if (!element || !className)
-    return
-
-  let classString = element.className
-  const nameIndex = classString.indexOf(className)
-  if (nameIndex === -1) {
-    classString += `${className}`
-  }
-  else {
-    classString
-      = classString.substr(0, nameIndex)
-      + classString.substr(nameIndex + className.length)
-  }
-  element.className = classString
 }
 
 /**
@@ -286,73 +223,18 @@ export function createUniqueString() {
 }
 
 /**
- * Check if an element has a class
- * @param {HTMLElement} elm
- * @param {string} cls
- * @returns {boolean}
+ * @param {string} url
+ * @returns {string}
  */
-export function hasClass(ele: HTMLElement, cls: string) {
-  return !!ele.className.match(new RegExp(`(\\s|^)${cls}(\\s|$)`))
-}
+export function resolvePath(...paths) {
+  // 实现类似于 path.resolve 的功能
+  return paths.reduce((prev, curr) => {
+    if (prev.endsWith('/') && curr.startsWith('/'))
+      return prev + curr.slice(1)
 
-/**
- * Add class to element
- * @param {HTMLElement} elm
- * @param {string} cls
- */
-export function addClass(ele: HTMLElement, cls: string) {
-  if (!hasClass(ele, cls))
-    ele.className += ` ${cls}`
-}
+    if (prev.endsWith('/') || curr.startsWith('/'))
+      return prev + curr
 
-/**
- * Remove class from element
- * @param {HTMLElement} elm
- * @param {string} cls
- */
-export function removeClass(ele: HTMLElement, cls: string) {
-  if (hasClass(ele, cls)) {
-    const reg = new RegExp(`(\\s|^)${cls}(\\s|$)`)
-    ele.className = ele.className.replace(reg, ' ')
-  }
-}
-
-/**
- * @param {Sting} url
- * @param {Sting} title
- * @param {Number} w
- * @param {Number} h
- */
-export default function openWindow(url, title, w, h) {
-  // Fixes dual-screen position                            Most browsers       Firefox
-  const dualScreenLeft
-    = window.screenLeft !== undefined ? window.screenLeft : window.screenX
-  const dualScreenTop
-    = window.screenTop !== undefined ? window.screenTop : window.screenY
-
-  const width = window.innerWidth
-    ? window.innerWidth
-    : document.documentElement.clientWidth
-      ? document.documentElement.clientWidth
-      : screen.width
-  const height = window.innerHeight
-    ? window.innerHeight
-    : document.documentElement.clientHeight
-      ? document.documentElement.clientHeight
-      : screen.height
-
-  const left = width / 2 - w / 2 + dualScreenLeft
-  const top = height / 2 - h / 2 + dualScreenTop
-  const newWindow = window.open(
-    url,
-    title,
-    `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, copyhistory=no, width=${w
-    }, height=${h
-    }, top=${top
-    }, left=${left}`,
-  ) as Window
-
-  // Puts focus on the newWindow
-
-  newWindow.focus()
+    return `${prev}/${curr}`
+  }).replace(/\/$/, '')
 }
