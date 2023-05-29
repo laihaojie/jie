0c97eded7b1d164831b0209701cfdd9b06d8366d
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-05-29 15:18:38 +0800

    feat: add nodejs utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,4 +2,8 @@ export * from './is'
 export * from './date'
 export * from './store'
 export * from './common'
+
+// 浏览器相关
 export * from './browser'
+// nodejs相关
+export * from './nodejs'
---
+++ packages/utils/src/nodejs.ts
@@ -0,0 +1,17 @@
+import os from 'node:os'
+
+/**
+ * Get ip(v4) address
+ * @return {String} the ipv4 address or 'localhost'
+ */
+export function getIPAddress(): string {
+  const interfaces = os.networkInterfaces()
+  let ip = ''
+  for (const dev in interfaces) {
+    interfaces[dev]?.forEach((details) => {
+      if (ip === '' && details.family === 'IPv4' && !details.internal)
+        ip = details.address
+    })
+  }
+  return ip || '127.0.0.1'
+}
