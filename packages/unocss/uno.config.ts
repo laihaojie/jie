0be51ec47285f8ac83673ee34684209db320a9b0
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-01-08 10:19:23 +0800

    chore: update

--- packages/unocss/uno.config.ts
+++ packages/unocss/uno.config.ts
@@ -1,25 +1,21 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
+import { defineConfig, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { presetWeb } from './preset/web'
 
 export default defineConfig({
+  content: {
+    pipeline: {
+      include: [
+        './**/*.ts',
+      ],
+    },
+  },
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
     ['fc', 'flex items-center justify-center'],
   ],
   presets: [
-    presetUno(),
-    presetAttributify(),
-    presetIcons({
-      scale: 1.2,
-      warn: true,
-    }),
+    presetWeb({ unit: 'px' }),
   ],
   transformers: [
     transformerDirectives(),
