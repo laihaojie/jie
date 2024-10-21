8a80d7ad8fdf836eba3fe465d303586c23cba923
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-10-21 10:39:54 +0800

    feat: unocss 支持动态配置

--- packages/unocss/preset/web.ts
+++ packages/unocss/preset/web.ts
@@ -1,5 +1,6 @@
 import type { Preset } from 'unocss'
 import type { PresetUnitOptions } from './unit'
+import { objectMerge } from '@djie/utils'
 import { presetAttributify, presetIcons, presetTypography, presetUno, presetWebFonts } from 'unocss'
 import { commonRules } from '../rules'
 import { commonShortcuts } from '../shortcuts'
@@ -7,27 +8,31 @@ import { commonTransformers } from '../transformers'
 import { presetUnit } from './unit'
 
 export interface PresetWebOptions extends PresetUnitOptions {
-
+  optionsPresetUno?: Parameters<typeof presetUno>[0]
+  optionsPresetAttributify?: Parameters<typeof presetAttributify>[0]
+  optionsPresetIcons?: Parameters<typeof presetIcons>[0]
+  optionsPresetTypography?: Parameters<typeof presetTypography>[0]
+  optionsPresetWebFonts?: Parameters<typeof presetWebFonts>[0]
 }
 
 export function presetWeb(options: PresetWebOptions = {}): Preset {
   return {
     name: '@djie/unocss-preset-web',
     presets: [
-      presetUno(),
-      presetAttributify(),
-      presetIcons({
+      presetUno(options.optionsPresetUno),
+      presetAttributify(options.optionsPresetAttributify),
+      presetIcons(objectMerge(options.optionsPresetIcons, {
         scale: 1.2,
         warn: true,
-      }),
-      presetTypography(),
-      presetWebFonts({
+      })),
+      presetTypography(options.optionsPresetTypography),
+      presetWebFonts(objectMerge(options.optionsPresetWebFonts, {
         fonts: {
           sans: 'DM Sans',
           serif: 'DM Serif Display',
           mono: 'DM Mono',
         },
-      }),
+      })),
       ...options.unit ? [presetUnit(options)] : [],
     ],
     shortcuts: [
